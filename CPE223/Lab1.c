60d03c6cba47715e96f37a353c06762204c80a21
Maimoona <maimoonaaz15@gmail.com>
2026-01-18 20:51:56 +0700

    Update Lab

--- CPE223/Lab1.c
+++ CPE223/Lab1.c
@@ -4,13 +4,15 @@
 
 #include <stdio.h>
 
-void signAndMagnitude(long long number, int bits, long long max);
-void onesCompliment(long long number, int bits, long long max);
+void signAndMagnitude(long long number, int bits, long long max, int isNegative);
+void onesCompliment(long long number, int bits, long long max, int isNegative);
 void twosCompliment(long long number, int bits, long long min, long long max);
 
 int main (void){
     int bits;
     long long min, max, number;
+    char input[100]; // Get input as string (In case of "-0")
+    int isNegative = 0;
 
     // Input the desired number of bits between 1 and 32 inclusively.
     printf("Enter the number of bits between 1 and 32: "); scanf("%i", &bits);
@@ -25,8 +27,16 @@ int main (void){
     min = -(1LL << (bits - 1));
     max = (1LL << (bits - 1)) - 1;
 
-    // Input decimal between min and max
-    printf("Enter a number between %lld and %lld: ", min, max); scanf("%lld", &number);
+    // Input decimal between min and max as a string
+    printf("Enter a number between %lld and %lld: ", min, max); scanf("%s", input);
+
+    // check if first character is "-"
+    if (input[0] == '-'){
+        isNegative = 1;
+    }
+
+    // Convert string input to number
+    sscanf(input, "%lld", &number);
 
     // Validate
     if (number < min || number > max){
@@ -35,42 +45,68 @@ int main (void){
     }
 
     // Print Results
-    //signAndMagnitude(number, bits, max);
-    onesCompliment(number, bits, max);
+    signAndMagnitude(number, bits, max, isNegative);
+    onesCompliment(number, bits, max, isNegative);
     //twosCompliment(number, bits, min, max);
 }
 
 
-void signAndMagnitude(long long number, int bits, long long max){
-    //unsigned int result; // Can change this
+void signAndMagnitude(long long number, int bits, long long max, int isNegative){
+    int binary[bits-1]; //array to store the results//
+    long long numCopy = number;
 
-    /* Code here
-        ... */
+    //initialize array to store binary number//
+    for (int i = 0; i < bits; i++){
+        binary[i] = 0;
+    }
 
-    //printf(result);
+    //checks if its a negative number//
+    if (isNegative){
+        number *= -1;
+        binary[0] = isNegative;
+    }
+
+    //converts the number to a binary number//
+    for (int i = bits-1; i > 0; i--){
+        binary[i] = number & 1;
+        number >>= 1;
+    }
+
+    if (number != 0) {
+        // If number is not 0, that means there is not enough bits.//
+        printf("The sign-magnitude doesn't represent %lld.\n", numCopy);
+
+    } else {
+        //prints the results//
+        printf("Sign-Magnitude: ");
+        for (int i = 0; i < bits ; i++){
+            printf("%d",binary[i]);
+        } printf("\n");
+    }
 }
 
 
-void onesCompliment(long long number, int bits, long long max){
+void onesCompliment(long long number, int bits, long long max, int isNegative){
     unsigned long long result;
 
+    // Check if it can be represented
     if (number < -max || number > max){
-        printf("1's complement cannot represent %lld\n", number);
+        printf("1's complement doesn't represent %lld\n", number);
         return;
     }
 
-    if (number >= 0) {
+    if (!isNegative) {
+        // Positive
         result = (unsigned long long)number;
     } else {
-
+        // Negative
         result = ~(unsigned long long)(-number);
     }
 
     result &= (1ULL << bits) - 1;
 
-
-    printf("1's complement: ");
-    for (int i = bits - 1; i >= 0; i--) {
+    printf("1's Complement: ");
+    for (int i = bits-1; i >= 0; i--) {
         printf("%d", (int)((result >> i) & 1));
 
     }
