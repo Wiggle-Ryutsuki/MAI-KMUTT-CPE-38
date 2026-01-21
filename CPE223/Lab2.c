659585ac61b768ab08b55ad48fa0003359b028d4
Maimoona <maimoonaaz15@gmail.com>
2026-01-21 23:47:53 +0700

    initialize

---
+++ CPE223/Lab2.c
@@ -0,0 +1,11 @@
+// Chanya Tinnaphopworachot 67070503456
+// Maimoona Aziz            67070503473
+// Iris French              67070503478
+
+
+// calculates the 44th Fibonacci number without using any special library. 
+#include <stdio.h>
+
+int main(void){
+    printf("Hello, world!");
+}
